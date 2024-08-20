2ce8e0e58f84a76a55694c646754548422bdf537
yuttakornk <yuttakorn.kaey@e-tech.ac.th>
2024-08-20 19:23:01 +0700

    Remove Duplicate

--- client/server/src/routes/userRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getUsers } from "../controllers/userController";
-
-const router = Router();
-
-router.get("/", getUsers); //http://localhost:8000/users
-
-export default router;
